19d9608722febd485981bc0c3700a54e413d5116
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2023-10-07 12:26:59 +0300

    authentication && dark light theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,20 @@
+import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { cn } from '@/lib/utils'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Discord',
   description: 'Discord Clone',
+  icons : {
+    icon : 
+      "/favicon.ico"
+    
+  }
 }
 
 export default function RootLayout({
@@ -15,8 +23,21 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(
+        inter.className,
+        "bg-white dark:bg-[#313338]"
+        )}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='dark'
+          enableSystem={false}
+          storageKey='discord-theme'>
+        {children}
+        </ThemeProvider>
+      </body>
     </html>
+    </ClerkProvider>
   )
 }
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <p>
-      This is discord clone please enjoy thank you
-    </p>
-  )
-}
